98c3aeea34a6cd16b87d59e861da5fef865bb953
Joseph Girdwood <j.girdwood@herts.ac.uk>
2019-01-18 11:45:55 +0000

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,21 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="AeroSAM_logger",
+    version="0.0.1",
+    author="Joseph Girdwood",
+    author_email="j.girdwood@herts.ac.uk",
+    description="Package for use on a raspberry pi zero, to log MET data from UH-AeroSAM1",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/JGirdwood/AeroSAM_logger",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 2.7",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: Linux",
+    ],
+)
